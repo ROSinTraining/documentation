57c7bee1ce78b7d5e00f9d2e609e127a09fe46a9
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2022-07-29 16:03:56 +0200

    update conf file

--- workshop/source/conf.py
+++ workshop/source/conf.py
@@ -14,7 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-
+import sphinx_rtd_theme
 # -- Project information -----------------------------------------------------
 
 project = 'ROS-I Training Workshop'
@@ -46,7 +46,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
